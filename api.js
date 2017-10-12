ee29bc47e99f894fce138037f2acf396def7806b
Student_01 <student_01@Student-19.local>
2017-10-12 14:38:19 +0700

    Third Commit

---
+++ api.js
@@ -0,0 +1,17 @@
+let rootURL = 'https://www.omdbapi.com/';
+
+exports.search = function(q){
+  let url =`${rootURL}?apikey=3f1288a7&s=${q}`;
+  console.log(url);
+  return fetch(url)
+    .then((resp)=> resp.json())
+    .then((json)=>{
+      return json.Search;
+    });
+}
+
+exports.view = function(id){
+  let url = `${rootURL}?apikey=3f1288a7&i=${id}&plot=short&r=json`;
+  return fetch(url)
+    .then((resp)=>resp.json());
+}
